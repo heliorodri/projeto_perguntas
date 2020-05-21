ff13dc54788f1de6c2f60827b13651fa8205c5fe
Hélio Junior Rodrigues da Silva <49084401+heliorodri@users.noreply.github.com>
2020-05-21 13:13:55 -0300

    Criando StateFull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main() => runApp(PerguntaApp());
 
-@override
-class PerguntaApp extends StatelessWidget {
-  final perguntas = [
-    'Qual o seu time do coração?',
-    'Qual jogador fez você gostar de futebol?'
-  ];
-  
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder(){
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+   }
+
+  @override  
   Widget build(BuildContext context){
+    final perguntas = [
+      'Qual o seu time do coração?',
+      'Qual jogador você gostaria de ter no seu time?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,18 +27,25 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(perguntas[0]),
+            Questao(perguntas[_perguntaSelecionada]),
             RaisedButton(
               child: Text('Grêmio'),
-              onPressed: null,  
+              onPressed: _responder    
             ),
             RaisedButton(
               child: Text('Inter'),
-              onPressed: null,  
+              onPressed: _responder
             )
           ],
         ),
       ),
     );
   }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+
 }
